fd56aab99f426d7f47d89ef96286d5e35fed6f47
sotaVMacc <sdvsotagpejad@gmail.com>
2025-02-13 18:50:39 +0200

    Added to cantp init

--- TheStart/TheStart_Z4_0/Sources/CanTP.c
+++ TheStart/TheStart_Z4_0/Sources/CanTP.c
@@ -294,7 +294,7 @@ void CanTP_init(dataFrame* sendingBuffer, dataFrame* recieveBuffer)
 void Can_init(can_instance_t* can_pal_instance, can_user_config_t* can_pal_Config)
 {
     can_instance = can_pal_instance;
-
+    CAN_Init(can_pal_instance, can_pal_Config);
     CAN_InstallEventCallback(can_pal_instance, interrupt_callback, NULL);
     can_buff_config_t buffConf = {false, false, 0xAA, CAN_MSG_ID_STD, false};
     CAN_ConfigRxBuff(&can_pal1_instance, RX_BUFF_NUM, &buffConf, 0x3);
