ce539c471a2601fb801db31c069e4f0b46699173
Mohamed Ouda <m.a.ouda2023@gmail.com>
2025-04-21 15:25:42 +0200

    Add decompression utilities to bootloader_utils
    Created bootloader_utils directory and added decompression utilities. The decompression wrapper utilizes the LZ4 library.

---
+++ Bootloader/bootloader_utils/bl_decompress/bl_decompress.h
@@ -0,0 +1,43 @@
+/***************************************************************************************
+ * Module Name  : Bootloader Decompress                                                *
+ * File Name    : bl_decompress.h                                                      *
+ * Author       : Ouda                                                                 *
+ * Purpose      : This module is a wrapper layer for decompression of incoming updates *
+ ***************************************************************************************/
+
+/***************************************************************************************
+ *                                   -=[Includes]=-                                    *
+ ***************************************************************************************/
+#include "../status.h"
+#include "lz4.h"
+
+/***************************************************************************************
+ *                                   -=[Definitions]=-                                 *
+ ***************************************************************************************/
+
+/***************************************************************************************
+ *                                    -=[Externs]=-                                    *
+ ***************************************************************************************/
+extern uint32_t g_BL_Decompress_UpdateBlockSize;
+/***************************************************************************************
+ *                                 -=[Function IDs]=-                                  *
+ ***************************************************************************************/
+#define BLDECOMP_DECOMP                 (1U)
+
+/***************************************************************************************
+ *                               -=[Function Prototypes]=-                             *
+ ***************************************************************************************/
+
+/***************************************************************************************
+ * Function Name: BLDecomp_Decompress                                                  *
+ * Inputs       : pa_u8source       - Pointer to compressed source data                *
+ *                pa_u8dest         - Pointer to destination buffer                    *
+ *                a_u32compressedSize - Size of compressed data in bytes               *
+ * Outputs      : uint32_t          - Number of bytes decompressed                     *
+ * Reentrancy   : Non-Reentrant                                                        *
+ * Synchronous  : Synchronous                                                          *
+ * Description  : Decompresses received update data using LZ4 algorithm.               *
+ ***************************************************************************************/
+status_t BLDecomp_Decompress(const uint8_t* pa_u8source,
+                             uint8_t* pa_u8dest,
+                             uint32_t a_u32compressedSize);
